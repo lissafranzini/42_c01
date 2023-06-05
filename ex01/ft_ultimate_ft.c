97639ac00744d4bbedf3f1ffb23b8206bf296987
ldellefr <lissa.dfranzini@gmail.com>
2023-06-05 15:27:18 -0300

    ex01

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldellefr <ldellefr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 15:10:51 by ldellefr          #+#    #+#             */
+/*   Updated: 2023/06/05 15:26:46 by ldellefr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	a;
+	int	*nbr1;
+	int	**nbr2;
+	int	***nbr3;
+	int	****nbr4;
+	int	*****nbr5;
+	int	******nbr6;
+	int	*******nbr7;
+	int	********nbr8;
+	int	*********nbr9;
+
+	a = 2;
+	nbr1 = &a;
+	nbr2 = &nbr1;
+	nbr3 = &nbr2;
+	nbr4 = &nbr3;
+	nbr5 = &nbr4;
+	nbr6 = &nbr5;
+	nbr7 = &nbr6;
+	nbr8 = &nbr7;
+	nbr9 = &nbr8;
+
+	printf("int a era %d\n", a);
+
+	ft_ultimate_ft(nbr9);
+
+	printf("int a ficou %d\n", a);
+}*/
