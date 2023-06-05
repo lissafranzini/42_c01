d985db0f69e6d90bb594c7f2ac6e48476f499173
ldellefr <lissa.dfranzini@gmail.com>
2023-06-05 17:14:42 -0300

    comments on ex00

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -6,9 +6,11 @@
 /*   By: ldellefr <ldellefr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 14:54:20 by ldellefr          #+#    #+#             */
-/*   Updated: 2023/06/05 15:05:42 by ldellefr         ###   ########.fr       */
+/*   Updated: 2023/06/05 17:14:20 by ldellefr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//nbr is a pointer to an int, function set value (*) of pointer as 42
 
 void	ft_ft(int *nbr)
 {
